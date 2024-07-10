9e4595beaf918e682b6ee774410d5fdd72f61870
Rucha Tare <tarerucha8@gmail.com>
2024-07-10 08:06:46 +0100

    docstrings updated

--- langchain_api/app.py
+++ langchain_api/app.py
@@ -1,7 +1,8 @@
 """
-Utility to use
+Utility to run the API server for the Langchain project using FastAPI, that serves the OpenAI GPT-3 and llama2 models.
 """
 
+import logging
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
@@ -12,20 +13,51 @@ from langchain_community.llms import Ollama
 from dotenv import load_dotenv
 
 load_dotenv()
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(FuncName)s - %(levelname)s - %(message)s"
+)
 
 
 def create_api() -> FastAPI:
+    """
+    Create the FastAPI instance.
+
+    Returns
+    -------
+    FastAPI
+        The FastAPI instance
+    """
     api = FastAPI(title="Langchain API Server", version="1.0", description="Test server ")
     return api
 
 
 def create_models():
+    """
+    Create the OpenAI and llama2 models instances.
+
+    Returns
+    -------
+    openai_model, llm_model
+        The OpenAI and llama2 models instances
+    """
     openai_model = ChatOpenAI()
     llm_model = Ollama(model="llama2")
     return openai_model, llm_model
 
 
 def setup_routes(api: FastAPI, openai_model: ChatOpenAI, llm_model: Ollama):
+    """
+    Setup the routes for the API server, using the OpenAI and llama2 models , prompts and api
+
+    Parameters
+    ----------
+    api : FastAPI
+        The FastAPI instance
+    openai_model : ChatOpenAI
+        The OpenAI model instance
+    llm_model : Ollama
+        The llama2 model instance
+    """
     prompt1 = ChatPromptTemplate.from_template(
         "Explain the concept {topic} with 5 bullet points only "
     )
@@ -38,10 +70,16 @@ def setup_routes(api: FastAPI, openai_model: ChatOpenAI, llm_model: Ollama):
 def main():
     """
     Main function to run the utility.
+
+    Calls the create_api : Creates the FastAPI instance.
+    create_models: Creates the OpenAI and llama2 models instances.
+    setup_routes : Setup the routes for the API server, using the OpenAI and llama2 models , prompts and api
     """
 
     api = create_api()
+
     openai_model, llm_model = create_models()
+
     setup_routes(api, openai_model, llm_model)
 
 
