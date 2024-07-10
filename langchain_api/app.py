d5516c4bc7736bdbfcd6020710dcb0aecb9381f9
Rucha Tare <tarerucha8@gmail.com>
2024-07-10 08:15:32 +0100

    📝 logging and imports

--- langchain_api/app.py
+++ langchain_api/app.py
@@ -5,7 +5,7 @@ Utility to run the API server for the Langchain project using FastAPI, that serv
 import logging
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langserve import add_routes
 import uvicorn
 import os
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(FuncName)s - %(levelname)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(funcName)s - %(levelname)s - %(message)s"
 )
 
 
@@ -27,6 +27,9 @@ def create_api() -> FastAPI:
     FastAPI
         The FastAPI instance
     """
+
+    logging.info("Creating FastAPI instance")
+
     api = FastAPI(title="Langchain API Server", version="1.0", description="Test server ")
     return api
 
@@ -40,6 +43,9 @@ def create_models():
     openai_model, llm_model
         The OpenAI and llama2 models instances
     """
+
+    logging.info("Creating OpenAI and llama2 models instances")
+
     openai_model = ChatOpenAI()
     llm_model = Ollama(model="llama2")
     return openai_model, llm_model
@@ -58,6 +64,9 @@ def setup_routes(api: FastAPI, openai_model: ChatOpenAI, llm_model: Ollama):
     llm_model : Ollama
         The llama2 model instance
     """
+
+    logging.info("Setting up routes for the API server")
+
     prompt1 = ChatPromptTemplate.from_template(
         "Explain the concept {topic} with 5 bullet points only "
     )
@@ -75,6 +84,8 @@ def main():
     create_models: Creates the OpenAI and llama2 models instances.
     setup_routes : Setup the routes for the API server, using the OpenAI and llama2 models , prompts and api
     """
+
+    logging.info("Running the utility to run the API server for the Langchain project")
 
     api = create_api()
 
